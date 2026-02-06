6a5a37b4f16fce26ef554b14496bd282d086aa85
Patrik Wallström <pawal@amplitut.de>
2026-02-06 23:11:21 +0100

    Bump to v0.9.15

--- engine/engine.go
+++ engine/engine.go
@@ -73,7 +73,7 @@ type LogEntry struct {
 var ErrNotImplemented = errors.New("engine not implemented")
 
 // Version is the semantic version for this build.
-var Version = "0.9.14"
+var Version = "0.9.15"
 
 // Commit is optionally set at build time using -ldflags.
 var Commit = ""

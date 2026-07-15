12d8331339b7868a12ade87bd5a651120f2c07c5
Patrik Wallström <pawal@amplitut.de>
2026-07-16 00:01:29 +0200

    server: add nullable latency columns to the snapshot entity views

--- server/store_sql_test.go
+++ server/store_sql_test.go
@@ -294,7 +294,7 @@ func TestRunMigrationsRecordsVersion(t *testing.T) {
 		}
 		versions = append(versions, v)
 	}
-	want := []int{1, 2, 3, 4}
+	want := []int{1, 2, 3, 4, 5}
 	if len(versions) != len(want) {
 		t.Fatalf("expected %d versions, got %d: %v", len(want), len(versions), versions)
 	}

f967b4e20f87375e34d3c23edc32a355e31c324f
Patrik Wallström <pawal@amplitut.de>
2026-08-01 23:59:22 +0200

    Prep for v1.6.4

--- engine/engine.go
+++ engine/engine.go
@@ -127,7 +127,7 @@ type LogEntry struct {
 var ErrNotImplemented = errors.New("engine not implemented")
 
 // Version is the semantic version for this build.
-var Version = "1.6.3"
+var Version = "1.6.4"
 
 // Commit is optionally set at build time using -ldflags.
 var Commit = ""
